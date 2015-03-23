0c9c2c4696c0b43a610b3f78c93a22dbea3ce3ce
Chris Buonocore <chrisdistrict@gmail.com>
2015-03-23 09:53:06 -0700

    Ready to add form for ROI calculation

--- client/js/roi_client.js
+++ client/js/roi_client.js
@@ -1,62 +1,46 @@
 
-Session.set('roi_amount',0);
-Session.set('current_template', 'dashboard');
-console.log('hi');
+Meteor.subscribe('Companies');
 
-$('#hire_number').change(function() {
-  console.log('hire number changed');
-  alert('hi');
-});
-
-$('#cost_per_hire').change(function() {
-  console.log('cost_per_hire changed');
-});
-
-function changeTemplate(name) {
-  Session.set('current_template', name);
-  console.log('Changed template to ' + name);
-}
-
-Template.content_area.helpers({
-  whichOne: function () {
-    return Session.get('current_template');
-    // note that we return a string - per http://docs.meteor.com/#template_dynamic
+Template.recent.helpers({
+  companies: function() {
+    return Companies.find({}, {sort: {createdAt: -1}});
   }
+
 });
 
 
-Template.dashboard.helpers({
-    savedPrice: function() {
-        return Session.get("roi_amount");
-    },
-    totalPrice: function(){
-       var price= 0;
-       var items= Items.find({});
-       items.forEach(function(item){
-           price+=item.price;
-       });
-       return price;
-    },
-    totalQty: function(){
-       return Items.find({}).count();
-    },
-    items:function(){
-      return Items.find({});
-    }
-});
+Meteor.methods({
+  addCompany: function (company_obj) {
+    // Make sure the user is logged in before inserting a task
+
+    Companies.insert(company_obj);
+  },
+  deleteCompany: function (company_id) {
+    Companies.remove(company_id);
+  }
+  
+  });
 
 Template.dashboard.events({
-    'submit form': function(event){
-    event.preventDefault();
-    var hire_num = $('#hire_number').val();
-    var cost_hire = $('#cost_per_hire').val();
-    console.log('hire_num: ' + hire_num);
-    var saved = Math.round(hire_num*cost_hire*.8);
-    console.log('saved money: ' + saved);
-    Session.set('roi_amount',saved);
+    "submit .new-company": function (event) {
+      // This function is called when the new task form is submitted
+      var company = {name: 'hi',employees: 100,'roi': 100}
+
+      Meteor.call("addCompany", company);
+
+      // Clear form
+      //event.target.text.value = "";
+
+      // Prevent default form submit
+      return false;
+    }
+  });
+
+Template.recent.events({
+  "click .roi-entry": function() {
+    console.log('You clicked: ' + this);
+    //route to a page that allows updates to the entry
   }
-
 })
-
 
 
--- server/roi_server.js
+++ server/roi_server.js
@@ -1,11 +1,9 @@
 
-Companies = new Mongo.Collection("companies")
 
+Schemas = {};
 
 Meteor.methods({
   addCompany: function (company, employees, roi) {
-
-
     Companies.insert({
       name: company,
       createdAt: new Date(),
@@ -14,7 +12,7 @@ Meteor.methods({
     });
   },
   deleteCompany: function (companyId) {
-    var company = Companies.findOne(companyId);
+    //var company = Companies.findOne(companyId);
 
     Companies.remove(companyId);
   }
@@ -22,3 +20,90 @@ Meteor.methods({
 });
 
 
+
+//Template.registerHelper("Schemas", Schemas);
+
+Schemas.Person = new SimpleSchema({
+  firstName: {
+    type: String,
+    index: 1,
+    unique: true
+  },
+  lastName: {
+    type: String,
+    optional: true
+  },
+  age: {
+    type: Number,
+    optional: true
+  }
+});
+
+Schemas.Company = new SimpleSchema({
+  name: {
+    type: String,
+    label: "Company Name",
+    max: 100
+  },
+  employees: {
+    type: Number,
+    label: "Employee Count"
+  },
+  industry: {
+    type: String,
+    label: "Industry",
+    max:100,
+    optional: true
+  },
+
+  num_recruiters: {
+    type: Number,
+    label: "Number of recruiters on staff",
+    optional: true
+  },
+
+  num_positions_per_recruiter: {
+    type: Number,
+    label: "Positions filled by a recruiter in one year",
+    optional: true
+  },
+  cost_recruiter: {
+    type: Number,
+    label: "Fully burdened annual cost for a recruiter",
+    optional: true
+  },
+  num_engineers: {
+    type: Number,
+    label: "Number of Engineers",
+    optional: true
+  },
+  roi: {
+    type: Number,
+    label: "Total ROI (%)",
+    optional: true
+  }
+
+});
+
+People.attachSchema(Schemas.Person);
+
+Companies.attachSchema(Schemas.Company);
+
+
+Meteor.publish(null, function () {
+  return People.find();
+});
+
+People.allow({
+  insert: function () {
+    return true;
+  },
+  remove: function () {
+    return true;
+  }
+});
+
+
+Meteor.publish("Companies", function () {
+    return Companies.find({});
+  });
