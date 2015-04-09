2868382827f3e38463593ae44bff5308b55ee2c3
Chris Buonocore <chrisdistrict@gmail.com>
2015-04-09 15:57:07 -0700

    First commit of game shell

--- server/roi_server.js
+++ server/roi_server.js
@@ -1,44 +1,5 @@
 
-
-
-
-Meteor.methods({
-  addCompany: function (company, employees, roi) {
-    Companies.insert({
-      name: company,
-      createdAt: new Date(),
-      employees: employees,
-      roi: roi
-    });
-  },
-  deleteCompany: function (companyId) {
-    //var company = Companies.findOne(companyId);
-
-    Companies.remove(companyId);
-  }
-
-});
-
-
-People.attachSchema(Schemas.Person);
-
-Companies.attachSchema(Schemas.Company);
-
-
-Meteor.publish(null, function () {
-  return People.find();
-});
-
-People.allow({
-  insert: function () {
-    return true;
-  },
-  remove: function () {
-    return true;
-  }
-});
-
-
 Meteor.publish("Companies", function () {
     return Companies.find({});
-  });
+});
+
